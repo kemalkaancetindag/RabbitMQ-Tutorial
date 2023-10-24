7c4945c9fb0eb3d041cc0f4acf460c248c93ee3a
Kemal Kaan Cetindag <kemalkaancetindag@192.168.1.6>
2023-10-24 19:04:26 +0300

    pu sub

---
+++ 3-rabbitmq-pub-sub/producer.py
@@ -0,0 +1,18 @@
+import pika
+from pika.exchange_type import ExchangeType
+
+connection_parameters = pika.ConnectionParameters('localhost')
+
+connection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.channel()
+
+channel.exchange_declare(exchange="pubsub", exchange_type=ExchangeType.fanout)
+
+message = "Hello I Want To Broadcast This Message"
+
+channel.basic_publish(exchange="pubsub", routing_key="", body=message)
+
+print(f"sent message: {message}")
+
+connection.close()
